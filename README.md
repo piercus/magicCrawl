2cee88c6e7700fc147855cf47709abf6c08e4619
Piercus <piercus@gmail.com>
2016-02-28 23:18:39 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # magicCrawl
 Crawl firstname stats from http://www.magicmaman.com/
+
+see [http://www.magicmaman.com/prenom/]
+
+### Presentation
+
+You're a Geek ?
+You're a future Daddy ?
+You want to have free statistics on french firstnames ?
+
+This repo is made for you !
+
+### For the mummies
+
+Take a nap, you should not be on this repo ! should you ?
+
+### Installation
+
+1. Install nodejs see [https://nodejs.org]
+2. Download the repo 
+3. Put the firstnames you want to crawl in **firstnames.json**
+4. Put the years you want to crawl in **years.json**
+5. In a console prompt `node index.js`
+
+NB : the magicmaman.com databases is not fully clean, some years are not retrieved
+
+
+### Output format
+
+It output a csv directly.
+If you want to crawl all the firstname database, there may be issues, so you might want to connect to a database server.
+
+### Contribute
+
+Fork me, i won't be famous anyway ;-)
+
+Or add issues if you want !
---
+++ index.js
@@ -0,0 +1,90 @@
+var fs = require("fs");
+var http = require("http");
+require("./curry.js");
+require("./objMapAsync.js");
+
+var firstnames = require("./firstnames.json");
+var years = require("./years.json");
+
+
+var buildUrl = function(year, firstname){
+  return "http://www.magicmaman.com/direct/prenom/datacartedefrance?prenom="+firstname+"&anneeDeb="+year+"&anneeFin="+year+"&format=json";
+};
+
+var get = function(year, firstname, cb){
+
+  var url = buildUrl(year, firstname);
+
+  http.get(url, function(res){
+      var body = '';
+
+      res.on('data', function(chunk){
+          body += chunk;
+      });
+
+      res.on('end', function(){
+          var response = JSON.parse(body);
+          cb(null, response)
+      });
+  }).on('error', function(e){
+      cb(e);
+  });
+};
+
+var onJSON = function(year, firstname, cb, err, json){
+
+  if(err){
+    return cb(err);
+  }
+
+  var res = {
+    year : year,
+    firstname : firstname,
+    perDeps : {}
+  }, deps = json["parDepartements"];
+
+  for(var dep in deps) if(deps.hasOwnProperty(dep)){
+    res.perDeps[dep] || (res.perDeps[dep] = {});
+    res.perDeps[dep] = deps[dep].total;
+  }
+
+  cb(null, res);
+};
+
+var globalFile = "firstname, year, departement, number\n";
+
+firstnames.mapAsync(function(firstname, index, cbFirstname){
+  years.mapAsync(function(year, index2, cbYear){
+
+    get(year, firstname, onJSON.curry(year, firstname, cbYear));
+
+  },{},function(errs, res){
+
+    if(errs){
+      cbFirstname(errs);
+    }
+
+    //console.log(res);
+
+    var fileJSON = {};
+
+    for (var i = 0; i < res.length; i++){
+      //console.log(res[i].year.toString());
+      fileJSON[res[i].year.toString()] = res[i].perDeps;
+      for(var dep in res[i].perDeps) if(res[i].perDeps.hasOwnProperty(dep)){
+        globalFile+=res[i].firstname+","+res[i].year+","+dep+","+res[i].perDeps[dep]+"\n";
+      }
+    }
+    
+    cbFirstname();
+  });
+}, {}, function(errs, res){
+  if(errs){
+    console.log("errors : ", errs);
+  }
+  fs.writeFileSync("db.csv", globalFile, "utf8");
+
+
+  console.log("finished");
+
+});
